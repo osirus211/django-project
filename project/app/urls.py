37800ceb2332f959a59aae33b2428a5931d0cc0e
osirus211 <scriptlance90@gmail.com>
2020-10-09 14:51:17 +0530

    first

---
+++ project/app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+app_name = 'app'
+
+urlpatterns = [
+    path('register',views.register,name='register'),
+    path('user_login',views.user_login,name='user_login'),
+
+]
---
+++ project/app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from app.forms import UserForm, UserProfileInfoForm
+
+
+# for login
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request,'app/index.html',{})
+
+
+
+@login_required
+def special(request):
+    return HttpResponse("You Are Logged In Nice")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user.password) # this hashash the password
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+    return render(request,'app/registration.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password) # it will authenticate the user
+        if user:
+            if user.is_active:
+                login(request,user) # is user account is active login user
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("ACCOUNT NOT ACTIVE")
+        else:
+            print("Some one tried to login but failed")# if you want print this
+            print("User name: {} and Password: {}".format(username,password))
+            return HttpResponse("Invalid login details")
+    else:
+        return render(request,'app/login.html',{})
